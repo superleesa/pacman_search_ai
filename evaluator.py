5351f15d5bba9f5d43aa440907c868f4ce33a4f4
Ethan <ewil32@student.monash.edu>
2023-08-21 13:19:52 +1000

    changed timeout to seconds. Catch exceptions default is now True

--- evaluator.py
+++ evaluator.py
@@ -15,32 +15,10 @@ def run(all_arguments: Dict):
     print(all_arguments)
     for command in linear_product(all_arguments):
         command.append("-q")
-        command.append("-c")
         subprocess.call(command)
 
 
-# def read_command(argv):
-#     usageStr = """
-#     USAGE:      python evaluator.py <options>
-#     EXAMPLES:   (1) python pacman.py -y
-#     """
-#     parser = OptionParser(usageStr)
-#     parser.add_option('-y', action='store_true', dest='overwrite_logs',
-#                       help='Confirm overwriting of previous logs', default=False)
-#     options, otherjunk = parser.parse_args(argv)
-#     if len(otherjunk) != 0:
-#         raise Exception('Command line input not understood: ' + str(otherjunk))
-    
-#     return options
-
-
 if __name__ == "__main__":
-
-    # args = read_command( sys.argv[1:] ) # Get game components based on input
-
-
-    # if not args.overwrite_logs:
-
 
     logs_dir = './logs/'
     logs = glob.glob(logs_dir + "*.log")
@@ -55,6 +33,7 @@ if __name__ == "__main__":
         "--agentArgs": ["fn=q1a_solver,prob=q1a_problem"],
         "--layout": question_1a_layouts,
         "--outfile": ["question_1a"],
+        "--timeout":["1"]
         }
     
     question_1b_pattern = "q1b_*.lay"
@@ -65,6 +44,7 @@ if __name__ == "__main__":
         "--agentArgs": ["fn=q1b_solver,prob=q1b_problem"],
         "--layout": question_1b_layouts,
         "--outfile": ["question_1b"],
+        "--timeout":["5"]
         }
     
     question_1c_pattern = "q1c_*.lay"
@@ -75,7 +55,7 @@ if __name__ == "__main__":
         "--agentArgs": ["fn=q1c_solver,prob=q1c_problem"],
         "--layout": question_1c_layouts,
         "--outfile": ["question_1c"],
-        "--timeout":["0.5"]
+        "--timeout":["30"]
         }
     
     question_2a_patterns = "q2a_*.lay"
@@ -85,7 +65,7 @@ if __name__ == "__main__":
         "--pacman": ["Q2A_Agent"],
         "--layout": question_2a_layouts,
         "--outfile": ["question_2a"],
-        "--timeout":["0.5"]
+        "--timeout":["30"]
         }
     
     question_2b_patterns = "q2b_*.lay"
@@ -95,7 +75,7 @@ if __name__ == "__main__":
         "--pacman": ["Q2B_Agent"],
         "--layout": question_2b_layouts,
         "--outfile": ["question_2b"],
-        "--timeout":["0.5"]
+        "--timeout":["30"]
         }
     
     run(question_1a_args)
